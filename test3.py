7389307b37ef4bb020567b3597df06fa06ae55bb
Patrick Hansen <pat@patricks-mbp.dhcp.nd.edu>
2016-04-23 12:35:40 -0400

    2016 analysis (test3)

--- test3.py
+++ test3.py
@@ -9,29 +9,50 @@ from textblob import TextBlob
 con = None
 
 try:
-    con = lite.connect('tweeter.db')
+    con = lite.connect('tweeter_2016.db')
     with con:
         array = []
         cur = con.cursor()    
 	count = 0
-        for row in cur.execute("SELECT TweetText FROM Tweets2012 WHERE (Lower(TweetText) LIKE '%romney%') AND (Lower(TweetText) NOT LIKE '%obama%')"):
+        for row in cur.execute("SELECT TweetText FROM Tweets2016 WHERE ((Lower(TweetText) LIKE '%sanders%') OR (Lower(TweetText) LIKE '%bernie%')) AND (Lower(TweetText) NOT LIKE '%trump%')"):
             count += 1
             array.append(row[0])
-            print row[0]
+            #print row[0]
         print count
 
     sentiments = []
+    subjects = []
     avg = 0
+    savg = 0
+    countp = 0
+    countn = 0
+    countt = 0
 
-    for i in range(60,100):
+    for i in range(len(array)):
 
         polarity = TextBlob(array[i]).sentiment.polarity
+        sub = TextBlob(array[i]).sentiment.subjectivity
 
+        if polarity > 0:
+            countp += 1
+        elif polarity < 0:
+            countn += 1
+        else:
+            countt += 1
+
+        subjects.append(sub)
         sentiments.append(polarity)
         avg += polarity
+        savg += sub
 
     avg /= len(sentiments)
-    print avg
+    savg /= len(subjects)
+    print 'avg sentiment: ', avg
+    print 'avg subjectiv: ', savg
+    print 'positive coun: ', countp
+    print 'negative coun: ', countn
+    print 'zero count   : ', countt
+    
     
 except lite.Error, e:
     
